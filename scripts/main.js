f960ded957cc78ad1b927c456b1883a31c944451
DepletedPrism <depletedprism@qq.com>
2020-02-15 13:35:03 +0800

    Site updated: 2020-02-15 13:35:02

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    $("div.fold").css("display", "none");
+});
+
 (function() {
   'use strict';
 
