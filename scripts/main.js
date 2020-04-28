855ee53b9855d08dd83a90eea0222dce23f089db
DepletedPrism <depletedprism@qq.com>
2020-04-29 00:42:18 +0800

    Site updated: 2020-04-29 00:42:17

--- scripts/main.js
+++ scripts/main.js
@@ -274,3 +274,24 @@ $(document).ready(function(){
     initPhotoSwipeFromDOM(gallerySelector);
   }
 })();
+
+// hexo-renderer-kramed
+
+MathJax.Hub.Config({
+  menuSettings: {
+    zoom: "None"
+  },
+  showMathMenu: false,
+  jax: ["input/TeX","output/CommonHTML"],
+  extensions: ["tex2jax.js"],
+  TeX: {
+    extensions: ["AMSmath.js","AMSsymbols.js"],
+    equationNumbers: {
+      autoNumber: "AMS"
+    }
+  },
+  tex2jax: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]]
+  }
+});
